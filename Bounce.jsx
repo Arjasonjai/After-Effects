ea55005b00432af98f4c85344a49689b03900da3
Arjasonjai <47569887+Arjasonjai@users.noreply.github.com>
2019-03-15 15:20:40 +0800

    Latest Version

--- Bounce.jsx
+++ Bounce.jsx
@@ -1,42 +1,22 @@
-n = 0;
- 
+n = 0; 
 if (numKeys > 0){
- 
 n = nearestKey(time).index;
- 
 if (key(n).time > time){
- 
 n--;
- 
 }
- 
 }
- 
 if (n == 0){
- 
 t = 0;
- 
 }else{
- 
 t = time - key(n).time;
- 
 }
- 
 if (n > 0){
- 
 v = velocityAtTime(key(n).time - thisComp.frameDuration/10);
- 
 amp = 0.25;
- 
 freq = 2;
- 
 decay = 4;
- 
 M=Math.sin(freq*t*2*Math.PI)/Math.exp(decay*t); 
 value + v*amp*M;
- 
 }else{
- 
 value;
- 
 }
