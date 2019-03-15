0c37081bf421aa81a0c9ea51ff2f3962d192babb
Arjasonjai <47569887+Arjasonjai@users.noreply.github.com>
2019-03-15 11:41:42 +0800

    Latest Version

--- Ease.jsx
+++ Ease.jsx
@@ -1,29 +1,34 @@
-
-// some defaults
-var p = 0.8;		// period for elastic
-var a = 50;		// amplitude for elastic
-var s = 1.70158;	// overshoot amount for "back"
-
-
-function Ease_outExpo(t, b, c, d) {
-	return (t==d) ? b+c : c * (-Math.pow(2, -10 * t/d) + 1) + b;
+function Ease_outBounce(t, b, c, d) {
+	if ((t/=d) < (1/2.75)) { return c*(7.5625*t*t) + b }
+	else if (t < (2/2.75)) { return c*(7.5625*(t-=(1.5/2.75))*t + .75) + b }
+	else if (t < (2.5/2.75)) { return c*(7.5625*(t-=(2.25/2.75))*t + .9375) + b } 
+	else { return c*(7.5625*(t-=(2.625/2.75))*t + .984375) + b }
 }
+ 
+function Ease_inBounce(t, b, c, d) {
+	return c - Ease_outBounce (d-t, 0, c, d) + b;
+}
+ 
+function Ease_inoutBounce(t, b, c, d) {
+	if (t < d/2) return Ease_inBounce (t*2, 0, c, d) * .5 + b;
+	else return Ease_outBounce (t*2-d, 0, c, d) * .5 + c*.5 + b;
+}
+ 
 
-
-function easeAndWizz() {
+function Ease() {
 	var n = 0;
 	if (numKeys > 0) {
 		n = nearestKey(time).index;
-		if (key(n).time > time)	{ n-- }
+		if (key(n).time > time) { n-- }
 	}
-
+ 
 	try {
 		var key1 = key(n);
 		var key2 = key(n+1);
 	} catch(e) {
 		return null;
 	}
-	
+ 
 	// determine how many dimensions the keyframes need
 	var dim = 1; // It's gotta have at least ONE dimension
 	try {
@@ -32,38 +37,38 @@ function easeAndWizz() {
 		key(1)[2];
 		dim = 3;
 	} catch(e) {}
-
+ 
 	t = time - key1.time;
 	d = key2.time - key1.time;
-
+ 
 	sX = key1[0];
 	eX = key2[0] - key1[0];
-
+ 
 	if (dim >= 2) {
 		sY = key1[1];
 		eY = key2[1] - key1[1];
-
+ 
 		if (dim >= 3) {
 			sZ = key1[2];
 			eZ = key2[2] - key1[2];
 		}
 	}
-
+ 
 	if ((time < key1.time) || (time > key2.time)) {
 		return value;
 	} else {
-		val1 =  easeandwizz_outExpo(t, sX, eX, d, a, p, s);
+		val1 =  Ease_inoutBounce(t, sX, eX, d);
 		switch (dim) {
 			case 1:
 			     return val1;
 			     break;
 			case 2:
-			     val2 = easeandwizz_outExpo(t, sY, eY, d, a, p, s);
+			     val2 = Ease_inoutBounce(t, sY, eY, d);
 			     return [val1, val2];
 			     break;
 			case 3:
-			     val2 = easeandwizz_outExpo(t, sY, eY, d, a, p, s);
-			     val3 = easeandwizz_outExpo(t, sZ, eZ, d, a, p, s);
+			     val2 = Ease_inoutBounce(t, sY, eY, d);
+			     val3 = Ease_inoutBounce(t, sZ, eZ, d);
 			     return [val1, val2, val3];
 			     break;
 			default:
@@ -71,6 +76,5 @@ function easeAndWizz() {
 		}
 	}
 }
-
+ 
 (Ease() || value);
-
