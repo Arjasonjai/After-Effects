fb31f4d46e1eaba7133d28e96e0fbb50fa1fe4f1
Arjasonjai <47569887+Arjasonjai@users.noreply.github.com>
2019-03-20 09:58:00 +0800

    Latest Version

---
+++ Different Functions.jsx
@@ -0,0 +1,33 @@
+// Apply Easing Starts
+
+function applyEasing(property, keyTimesArray, easeInArray, easeOutArray, keyInterpolationArray) {
+	var easeIn, easeOut, easeIn0, easeOut0, easeIn1, easeOut1, easeIn2, easeOut2;
+	for (var i = 0, il = keyTimesArray.length; i < il; i ++) {
+		if (property.propertyValueType === PropertyValueType.TwoD) {
+			easeIn0 = new KeyframeEase(easeInArray[0][i][0], easeInArray[1][i][0]);
+			easeOut0 = new KeyframeEase(easeOutArray[0][i][0], easeOutArray[1][i][0]);
+			easeIn1 = new KeyframeEase(easeInArray[0][i][1], easeInArray[1][i][1]);
+			easeOut1 = new KeyframeEase(easeOutArray[0][i][1], easeOutArray[1][i][1]);
+			property.setTemporalEaseAtKey(i+1, [easeIn0, easeIn1], [easeOut0, easeOut1]);
+		} else if (property.propertyValueType === PropertyValueType.ThreeD) {
+			easeIn0 = new KeyframeEase(easeInArray[0][i][0], easeInArray[1][i][0]);
+			easeOut0 = new KeyframeEase(easeOutArray[0][i][0], easeOutArray[1][i][0]);
+			easeIn1 = new KeyframeEase(easeInArray[0][i][1], easeInArray[1][i][1]);
+			easeOut1 = new KeyframeEase(easeOutArray[0][i][1], easeOutArray[1][i][1]);
+			easeIn2 = new KeyframeEase(easeInArray[0][i][2], easeInArray[1][i][2]);
+			easeOut2 = new KeyframeEase(easeOutArray[0][i][2], easeOutArray[1][i][2]);
+			property.setTemporalEaseAtKey(i+1, [easeIn0, easeIn1, easeIn2], [easeOut0, easeOut1, easeOut2]);
+		} else {
+			easeIn = new KeyframeEase(easeInArray[0][i], easeInArray[1][i]);
+			easeOut = new KeyframeEase(easeOutArray[0][i], easeOutArray[1][i]);
+			if (keyInterpolationArray[1][i] !== KeyframeInterpolationType.HOLD) {
+				property.setTemporalEaseAtKey(i+1, [easeIn], [easeOut]);
+			} else {
+				property.setTemporalEaseAtKey(i+1, [easeIn]);
+			}
+		}
+		property.setInterpolationTypeAtKey(i+1, keyInterpolationArray[0][i], keyInterpolationArray[1][i]);
+	}
+}
+
+// Apply Easing Ends
